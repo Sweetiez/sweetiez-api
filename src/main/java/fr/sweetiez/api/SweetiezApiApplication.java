1bfd86967703f1cefcb40ea6577f30c44ace7494
chukitipok <antonin.texier@gmail.com>
2022-03-04 12:53:38 +0100

    Init spring project with web and test dependencies.

---
+++ src/main/java/fr/sweetiez/api/SweetiezApiApplication.java
@@ -0,0 +1,13 @@
+package fr.sweetiez.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetiezApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetiezApiApplication.class, args);
+	}
+
+}
