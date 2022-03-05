3a4105342fdd94198a3f8f3ec318c312bb9daecb
chukitipok <antonin.texier@gmail.com>
2022-03-05 19:30:59 +0100

    Validate steps of cucumber scenario.

--- src/test/java/fr/sweetiez/api/acceptance/steps/CartSteps.java
+++ src/test/java/fr/sweetiez/api/acceptance/steps/CartSteps.java
@@ -1,13 +1,17 @@
 package fr.sweetiez.api.acceptance.steps;
 
 import io.cucumber.java8.En;
-import io.cucumber.java8.PendingException;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CartSteps implements En {
 
     public CartSteps() {
-        And("^the cart value is \"([^\"]*)\" with \"([^\"]*)\" items remaining$", (String arg0, String arg1) -> {
-            throw new PendingException();
+        And("^the cart value is \"([^\"]*)\" with \"([^\"]*)\" items remaining$", (BigDecimal price, Long quantity) -> {
+            assertTrue(price.compareTo(BigDecimal.valueOf(25)) > 0);
+            assertTrue(quantity >= 30);
         });
     }
 }
