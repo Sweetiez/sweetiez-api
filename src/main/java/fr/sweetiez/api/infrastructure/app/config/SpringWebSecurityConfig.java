2858191643db9449a6eb8d8d510340cd844c189c
chukitipok <antonin.texier@gmail.com>
2022-05-13 13:37:03 +0200

    feat(security): fix dependencies issues

--- src/main/java/fr/sweetiez/api/infrastructure/app/config/SpringWebSecurityConfig.java
+++ src/main/java/fr/sweetiez/api/infrastructure/app/config/SpringWebSecurityConfig.java
@@ -1,15 +1,11 @@
 package fr.sweetiez.api.infrastructure.app.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
@@ -19,19 +15,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.List;
 
 @Configuration
-@EnableWebSecurity
 public class SpringWebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private final UserDetailsService jwtUserDetailsService;
-
-    public SpringWebSecurityConfig(UserDetailsService jwtUserDetailsService) {
-        this.jwtUserDetailsService = jwtUserDetailsService;
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
