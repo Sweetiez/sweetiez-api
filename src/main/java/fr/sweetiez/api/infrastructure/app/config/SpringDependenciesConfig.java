4442f7508cebcc781b1a7169c2fda7623eaefe15
chukitipok <antonin.texier@gmail.com>
2022-05-21 21:38:13 +0200

    feat(infra): add new dependencies

--- src/main/java/fr/sweetiez/api/infrastructure/app/config/SpringDependenciesConfig.java
+++ src/main/java/fr/sweetiez/api/infrastructure/app/config/SpringDependenciesConfig.java
@@ -1,24 +1,33 @@
 package fr.sweetiez.api.infrastructure.app.config;
 
+import fr.sweetiez.api.adapter.delivery.AuthenticationEndPoints;
 import fr.sweetiez.api.adapter.delivery.EvaluationEndPoints;
 import fr.sweetiez.api.adapter.delivery.SweetEndPoints;
 import fr.sweetiez.api.adapter.repository.*;
-import fr.sweetiez.api.adapter.shared.EvaluationMapper;
+import fr.sweetiez.api.adapter.shared.AccountMapper;
 import fr.sweetiez.api.adapter.shared.CustomerMapper;
+import fr.sweetiez.api.adapter.shared.EvaluationMapper;
 import fr.sweetiez.api.adapter.shared.SweetMapper;
+import fr.sweetiez.api.core.authentication.ports.AuthenticationRepository;
+import fr.sweetiez.api.core.authentication.services.AuthenticationService;
+import fr.sweetiez.api.core.customers.ports.CustomerReader;
+import fr.sweetiez.api.core.customers.ports.CustomerWriter;
+import fr.sweetiez.api.core.customers.services.CustomerService;
 import fr.sweetiez.api.core.evaluations.ports.EvaluationReader;
 import fr.sweetiez.api.core.evaluations.ports.EvaluationWriter;
 import fr.sweetiez.api.core.evaluations.services.EvaluationService;
-import fr.sweetiez.api.core.customers.ports.CustomerReader;
-import fr.sweetiez.api.core.customers.services.CustomerService;
 import fr.sweetiez.api.core.sweets.ports.SweetsReader;
 import fr.sweetiez.api.core.sweets.ports.SweetsWriter;
 import fr.sweetiez.api.core.sweets.services.SweetService;
-import fr.sweetiez.api.infrastructure.repository.evaluations.EvaluationRepository;
+import fr.sweetiez.api.infrastructure.app.security.TokenProvider;
+import fr.sweetiez.api.infrastructure.repository.accounts.AccountRepository;
+import fr.sweetiez.api.infrastructure.repository.accounts.RoleRepository;
 import fr.sweetiez.api.infrastructure.repository.customers.CustomerRepository;
+import fr.sweetiez.api.infrastructure.repository.evaluations.EvaluationRepository;
 import fr.sweetiez.api.infrastructure.repository.sweets.SweetRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 @Configuration
 public class SpringDependenciesConfig {
@@ -26,11 +35,24 @@ public class SpringDependenciesConfig {
     private final SweetRepository sweetRepository;
     private final EvaluationRepository evaluationRepository;
     private final CustomerRepository customerRepository;
+    private final AccountRepository accountRepository;
+    private final RoleRepository roleRepository;
 
-    public SpringDependenciesConfig(SweetRepository sweetRepository, EvaluationRepository evaluationRepository, CustomerRepository customerRepository) {
+    private final TokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationManager;
+
+    public SpringDependenciesConfig(SweetRepository sweetRepository, EvaluationRepository evaluationRepository,
+                                    CustomerRepository customerRepository, AccountRepository accountRepository,
+                                    RoleRepository roleRepository, TokenProvider tokenProvider,
+                                    AuthenticationManagerBuilder authenticationManager)
+    {
         this.sweetRepository = sweetRepository;
         this.evaluationRepository = evaluationRepository;
         this.customerRepository = customerRepository;
+        this.accountRepository = accountRepository;
+        this.roleRepository = roleRepository;
+        this.tokenProvider = tokenProvider;
+        this.authenticationManager = authenticationManager;
     }
 
     @Bean
@@ -54,8 +76,13 @@ public class SpringDependenciesConfig {
     }
 
     @Bean
+    public AccountMapper accountMapper() {
+        return new AccountMapper();
+    }
+
+    @Bean
     public CustomerMapper customerMapper() {
-        return new CustomerMapper();
+        return new CustomerMapper(accountMapper());
     }
 
     @Bean
@@ -64,8 +91,13 @@ public class SpringDependenciesConfig {
     }
 
     @Bean
+    public CustomerWriter customerWriter() {
+        return new CustomerWriterAdapter(customerRepository, customerMapper());
+    }
+
+    @Bean
     public CustomerService customerService() {
-        return new CustomerService(customerReader());
+        return new CustomerService(customerReader(), customerWriter());
     }
 
     @Bean
@@ -95,5 +127,18 @@ public class SpringDependenciesConfig {
     @Bean
     public SweetEndPoints sweetEndPoints() {
         return new SweetEndPoints(sweetService());
+    }
+
+    @Bean
+    public AuthenticationRepository authenticationRepository() {
+        return new AccountRepositoryAdapter(accountRepository, roleRepository, accountMapper());
+    }
+    @Bean
+    public AuthenticationService authenticationService() {
+        return new AuthenticationService(authenticationRepository(), customerService());
+    }
+    @Bean
+    public AuthenticationEndPoints authenticationEndPoints() {
+        return new AuthenticationEndPoints(tokenProvider, authenticationManager, authenticationService());
     }
 }
