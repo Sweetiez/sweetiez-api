d243c4bd4c03e7c9fb903535b99fc0f57b955261
chukitipok <antonin.texier@gmail.com>
2022-05-24 12:12:26 +0200

    feat(core): add method to verify if evaluation exists

--- src/main/java/fr/sweetiez/api/core/evaluations/ports/EvaluationReader.java
+++ src/main/java/fr/sweetiez/api/core/evaluations/ports/EvaluationReader.java
@@ -1,10 +1,12 @@
 package fr.sweetiez.api.core.evaluations.ports;
 
 import fr.sweetiez.api.core.evaluations.models.Evaluation;
+import fr.sweetiez.api.core.evaluations.models.EvaluationId;
 
 import java.util.Collection;
 import java.util.UUID;
 
 public interface EvaluationReader {
     Collection<Evaluation> retrieveAllBySubject(UUID subject);
+    boolean exists(EvaluationId evaluationId);
 }
