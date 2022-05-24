3a8de57b2186a9f724d0ea0e3c76b2d859405015
chukitipok <antonin.texier@gmail.com>
2022-05-24 12:13:12 +0200

    feat(core): add method to save a report

--- src/main/java/fr/sweetiez/api/adapter/repository/EvaluationWriterAdapter.java
+++ src/main/java/fr/sweetiez/api/adapter/repository/EvaluationWriterAdapter.java
@@ -2,21 +2,30 @@ package fr.sweetiez.api.adapter.repository;
 
 import fr.sweetiez.api.adapter.shared.EvaluationMapper;
 import fr.sweetiez.api.core.evaluations.models.Evaluation;
+import fr.sweetiez.api.core.evaluations.models.Report;
 import fr.sweetiez.api.core.evaluations.ports.EvaluationWriter;
 import fr.sweetiez.api.infrastructure.repository.evaluations.EvaluationRepository;
+import fr.sweetiez.api.infrastructure.repository.evaluations.ReportRepository;
 
 public class EvaluationWriterAdapter implements EvaluationWriter {
 
     private final EvaluationRepository evaluationRepository;
+    private final ReportRepository reportRepository;
     private final EvaluationMapper evaluationMapper;
 
-    public EvaluationWriterAdapter(EvaluationRepository evaluationRepository, EvaluationMapper evaluationMapper) {
+    public EvaluationWriterAdapter(EvaluationRepository evaluationRepository, ReportRepository reportRepository, EvaluationMapper evaluationMapper) {
         this.evaluationRepository = evaluationRepository;
+        this.reportRepository = reportRepository;
         this.evaluationMapper = evaluationMapper;
     }
 
     public Evaluation save(Evaluation evaluation) {
         var entity = evaluationRepository.save(evaluationMapper.toEntity(evaluation));
+        return evaluationMapper.toDto(entity);
+    }
+
+    public Report save(Report report) {
+        var entity = reportRepository.save(evaluationMapper.toEntity(report));
         return evaluationMapper.toDto(entity);
     }
 }
--- src/main/java/fr/sweetiez/api/core/evaluations/ports/EvaluationWriter.java
+++ src/main/java/fr/sweetiez/api/core/evaluations/ports/EvaluationWriter.java
@@ -1,7 +1,9 @@
 package fr.sweetiez.api.core.evaluations.ports;
 
+import fr.sweetiez.api.core.evaluations.models.Report;
 import fr.sweetiez.api.core.evaluations.models.Evaluation;
 
 public interface EvaluationWriter {
     Evaluation save(Evaluation evaluation);
+    Report save(Report evaluation);
 }
