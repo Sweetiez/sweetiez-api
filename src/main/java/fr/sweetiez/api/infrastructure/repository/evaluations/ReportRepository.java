41155cfe4502a77cf22174d4bb9ae69dc8a24ac2
chukitipok <antonin.texier@gmail.com>
2022-05-24 12:09:24 +0200

    feat(infra): add report persistence data

---
+++ src/main/java/fr/sweetiez/api/infrastructure/repository/evaluations/ReportRepository.java
@@ -0,0 +1,7 @@
+package fr.sweetiez.api.infrastructure.repository.evaluations;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReportRepository extends JpaRepository<ReportEntity, Long> {}
