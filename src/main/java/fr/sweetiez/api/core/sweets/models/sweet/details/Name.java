c8134cb3d0c2ae001baeb336212ae9589a8ea8ea
Gwendal <gwendou95@hotmail.fr>
2022-05-25 17:17:09 +0200

    fix(sweet): enable accents in name

--- src/main/java/fr/sweetiez/api/core/sweets/models/sweet/details/Name.java
+++ src/main/java/fr/sweetiez/api/core/sweets/models/sweet/details/Name.java
@@ -2,6 +2,6 @@ package fr.sweetiez.api.core.sweets.models.sweet.details;
 
 public record Name(String value) {
     public boolean isValid() {
-        return value != null && !value.isEmpty() && value.matches("^[A-Z][ A-Za-z]+$");
+        return value != null && !value.isEmpty() && value.matches("^[A-Z][ A-zÀ-ú]+$");
     }
 }
