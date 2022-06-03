28c4357e2cee0e20b6183bdf5bcf31d6033ff636
LAymeric <aymeric.larvet@gmail.com>
2022-06-03 18:55:34 +0200

    feat(api): adding id for customer

--- src/main/java/fr/sweetiez/api/core/customers/models/responses/UserProfileResponse.java
+++ src/main/java/fr/sweetiez/api/core/customers/models/responses/UserProfileResponse.java
@@ -1,8 +1,10 @@
 package fr.sweetiez.api.core.customers.models.responses;
 
 import fr.sweetiez.api.core.customers.models.Customer;
+import fr.sweetiez.api.core.customers.models.CustomerId;
 
 public record UserProfileResponse(
+        String id,
         String firstName,
         String lastName,
         String email,
@@ -10,7 +12,7 @@ public record UserProfileResponse(
 ) {
 
     public  UserProfileResponse(Customer customer) {
-        this(customer.firstName(), customer.lastName(), customer.email(), customer.phone());
+        this(customer.id().value(), customer.firstName(), customer.lastName(), customer.email(), customer.phone());
     }
 
 }
