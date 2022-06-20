737d71b3c458227f71628d884e83ae7fcdf9e67c
Gwendal <gwendou95@hotmail.fr>
2022-06-20 21:03:26 +0200

    feat(reward): create reward

--- src/main/java/fr/sweetiez/api/core/loyalty/rewards/ports/RewardsWriter.java
+++ src/main/java/fr/sweetiez/api/core/loyalty/rewards/ports/RewardsWriter.java
@@ -1,4 +1,8 @@
 package fr.sweetiez.api.core.loyalty.rewards.ports;
 
+import fr.sweetiez.api.core.loyalty.rewards.models.rewards.Reward;
+
 public interface RewardsWriter {
+
+    Reward save(Reward reward);
 }
