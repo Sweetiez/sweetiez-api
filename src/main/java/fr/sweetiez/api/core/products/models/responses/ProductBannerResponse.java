53a74b44801c9e6301d9eda2243f054807a22895
chukitipok <antonin.texier@gmail.com>
2022-06-20 19:02:31 +0200

    add(sweet): add type in return banner response

--- src/main/java/fr/sweetiez/api/core/products/models/responses/ProductBannerResponse.java
+++ src/main/java/fr/sweetiez/api/core/products/models/responses/ProductBannerResponse.java
@@ -1,6 +1,7 @@
 package fr.sweetiez.api.core.products.models.responses;
 
 import fr.sweetiez.api.core.products.models.Product;
+import fr.sweetiez.api.core.products.models.Sweet;
 
 import java.util.UUID;
 
@@ -8,13 +9,15 @@ import java.util.UUID;
 public record ProductBannerResponse(
         UUID id,
         String name,
-        String images) {
+        String images,
+        String type) {
 
     public ProductBannerResponse(Product product) {
         this(
                 product.id().value(),
                 product.name().value(),
-                product.details().images().toArray()[0].toString()
+                product.details().images().toArray()[0].toString(),
+                product instanceof Sweet ? "sweet" : "tray"
         );
     }
 }
