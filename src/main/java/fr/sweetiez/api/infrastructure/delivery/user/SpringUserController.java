6a978c0be9e9cd03fb3ec2d3cdfba95bc481df26
chukitipok <antonin.texier@gmail.com>
2022-06-21 16:11:40 +0200

    fix(app): fix missing endpoint

--- src/main/java/fr/sweetiez/api/infrastructure/delivery/user/SpringUserController.java
+++ src/main/java/fr/sweetiez/api/infrastructure/delivery/user/SpringUserController.java
@@ -1,14 +1,13 @@
 package fr.sweetiez.api.infrastructure.delivery.user;
 
 import fr.sweetiez.api.adapter.delivery.user.UserEndPoints;
+import fr.sweetiez.api.core.customers.models.UpdateCustomerRequest;
 import fr.sweetiez.api.core.customers.models.responses.UserProfileResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Repository
+@RestController
 @RequestMapping("/user")
 public class SpringUserController {
 
@@ -21,5 +20,10 @@ public class SpringUserController {
     @GetMapping("/me")
     public ResponseEntity<UserProfileResponse> getMyProfile(Authentication authentication) {
         return userEndPoints.getProfile(authentication.getName());
+    }
+
+    @PutMapping("/me")
+    public ResponseEntity<Object> updateMyProfile(@RequestBody UpdateCustomerRequest request) {
+        return userEndPoints.updateProfile(request);
     }
 }
