9fa0b9b709f93be929d916e38380e528bb24b6ed
Gwendal <gwendou95@hotmail.fr>
2022-06-27 18:47:08 +0200

    feat(mailing): client order mail

--- src/main/java/fr/sweetiez/api/core/orders/models/responses/ProductOrderedResponse.java
+++ src/main/java/fr/sweetiez/api/core/orders/models/responses/ProductOrderedResponse.java
@@ -1,3 +1,10 @@
 package fr.sweetiez.api.core.orders.models.responses;
 
-public record ProductOrderedResponse(String name, Integer quantity) {}
+import java.math.BigDecimal;
+
+public record ProductOrderedResponse(String name,
+                                     Integer quantity,
+                                     Integer unitPerPackage,
+                                     BigDecimal unitPrice,
+                                     BigDecimal total
+) {}
