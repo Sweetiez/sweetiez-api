e4db9b5d36d6bcc20160782a72334d3bf0caecb4
Gwendal <gwendou95@hotmail.fr>
2022-07-02 17:13:32 +0200

    feat(events): add get spaces and get animators

--- src/main/java/fr/sweetiez/api/adapter/repository/events/AnimatorsAdapter.java
+++ src/main/java/fr/sweetiez/api/adapter/repository/events/AnimatorsAdapter.java
@@ -4,23 +4,27 @@ import fr.sweetiez.api.core.events.animator.Animator;
 import fr.sweetiez.api.core.events.animator.AnimatorID;
 import fr.sweetiez.api.core.events.animator.Animators;
 import fr.sweetiez.api.core.events.schedule.Schedule;
+import fr.sweetiez.api.core.events.use_case.face_to_face.models.AnimatorAdminResponse;
+import fr.sweetiez.api.infrastructure.repository.accounts.AccountRepository;
+import fr.sweetiez.api.infrastructure.repository.accounts.RoleEntity;
 import fr.sweetiez.api.infrastructure.repository.customers.CustomerRepository;
 import fr.sweetiez.api.infrastructure.repository.events.animator.AnimatorRepository;
 import fr.sweetiez.api.infrastructure.repository.events.animator.ReservedAnimatorEntity;
 
 import java.time.Duration;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class AnimatorsAdapter implements Animators {
+    private final AccountRepository accountRepository;
     private final CustomerRepository customerRepository;
     private final AnimatorRepository animatorRepository;
 
-    public AnimatorsAdapter(AnimatorRepository animatorRepository, CustomerRepository customerRepository) {
+    public AnimatorsAdapter(AnimatorRepository animatorRepository, CustomerRepository customerRepository,
+                            AccountRepository accountRepository) {
         this.customerRepository = customerRepository;
         this.animatorRepository = animatorRepository;
+        this.accountRepository = accountRepository;
     }
 
     public Optional<Animator> findById(UUID id) {
@@ -43,6 +47,17 @@ public class AnimatorsAdapter implements Animators {
                 animator.getId().getAnimatorId(),
                 schedule.getStart(),
                 Duration.between(schedule.getStart(), schedule.getEnd())));
+    }
+
+    @Override
+    public Collection<AnimatorAdminResponse> findAll() {
+//        var animatorRole = new RoleEntity(3L, "ANIMATOR");
+        return accountRepository.findByRoles_Name("ANIMATOR").stream()
+                .map(account -> new AnimatorAdminResponse(customerRepository.findByEmail(account.getUsername()).orElseThrow().getId(),
+                        account.getUsername()
+                        )
+                )
+                .toList();
     }
 
     public void reschedule(Animator animator, Schedule schedule) {
--- src/main/java/fr/sweetiez/api/core/events/space/SpaceDTO.java
+++ src/main/java/fr/sweetiez/api/core/events/space/SpaceDTO.java
@@ -1,5 +1,7 @@
 package fr.sweetiez.api.core.events.space;
 
+import fr.sweetiez.api.core.events.use_case.face_to_face.models.CreateSpaceRequest;
+
 import java.util.UUID;
 
 public record SpaceDTO(
@@ -8,4 +10,10 @@ public record SpaceDTO(
        String city,
        String zipCode,
        Integer places
-) {}
+) {
+
+    public SpaceDTO(CreateSpaceRequest request) {
+        this(UUID.randomUUID(), request.address(), request.city(), request.zipCode(), request.places());
+    }
+
+}
