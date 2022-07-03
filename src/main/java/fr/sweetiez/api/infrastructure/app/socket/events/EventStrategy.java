4530793cddeeeeac0d589d788a3d34a6d35d950a
chukitipok <antonin.texier@gmail.com>
2022-07-03 18:59:18 +0200

    feat(app): manage room for video calls

--- src/main/java/fr/sweetiez/api/infrastructure/app/socket/events/EventStrategy.java
+++ src/main/java/fr/sweetiez/api/infrastructure/app/socket/events/EventStrategy.java
@@ -4,9 +4,6 @@ import fr.sweetiez.api.infrastructure.app.socket.events.models.Rooms;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 
 public interface EventStrategy {
     Rooms execute(WebSocketSession session, Object payload) throws IOException;
--- src/main/java/fr/sweetiez/api/infrastructure/app/socket/events/ReturnSignalStrategy.java
+++ src/main/java/fr/sweetiez/api/infrastructure/app/socket/events/ReturnSignalStrategy.java
@@ -24,28 +24,14 @@ public class ReturnSignalStrategy implements EventStrategy {
 
         var emitters = rooms.users().get(data.roomID());
 
-        System.out.println("SESSION");
-        System.out.println(session.getId());
-        System.out.println("USERS IN ROOM:");
-        System.out.println(emitters);
-
-        System.out.println("CALLER ID: ");
-        System.out.println(data.callerID());
-
-        var optionalEmitter = emitters.stream()
+        var emitter = emitters.stream()
                 .filter(webSocketSession -> webSocketSession.getId().equals(data.callerID()))
-                .findFirst();
+                .findFirst()
+                .orElseThrow();
 
-
-        if (optionalEmitter.isPresent()) {
-            System.out.println("I WAS HERE !");
             var messageData = new ReceivingReturnedSignalEventData(data.signal(), session.getId());
             var message = new Payload("receiving returned signal", messageData);
-            optionalEmitter.get().sendMessage(new TextMessage(mapper.writeValueAsString(message)));
-        }
-        else {
-            System.out.println("F**K");
-        }
+            emitter.sendMessage(new TextMessage(mapper.writeValueAsString(message)));
 
         return rooms;
     }
